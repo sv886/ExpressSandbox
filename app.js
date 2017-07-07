5e0b6429559d787bcfbec8ddf7b86812351b570b
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-07-07 18:25:21 -0400

    routing/local server 101

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ var express = require("express");
 var app = express();
 
 // app.whateverexpresscomeswith
+
+// ########################################################
+// Routes
+// ########################################################
+
+// "/" => "Hai there!"
+app.get("/", function(req, res){
+  res.send("Hai there!");
+});
+// "/adieu" => "adios cabrón"
+app.get("/adieu", function(req, res){
+  res.send("adios cabrón");
+});
+// "/dog" => "MEOW"
+app.get("/dog", function(req, res){
+  res.send("MEOW");
+});
+
+
+// Tell express to listen for requests (start server)
+
+app.listen(3000, function(){
+  console.log("engine rennin good too boy i tell u wHat");
+});
