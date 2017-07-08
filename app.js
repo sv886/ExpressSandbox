7ffb42e2cfb3c3d839194a43f5d0ef2986a61842
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-07-08 12:23:03 -0400

    add spaces btwn routes

--- app.js
+++ app.js
@@ -13,22 +13,27 @@ var app = express();
 app.get("/", function(req, res){
   res.send("Hai there!");
 });
+
 // "/adieu" => "adios cabrón"
 app.get("/adieu", function(req, res){
   res.send("adios cabrón");
 });
+
 // "/dog" => "MEOW"
 app.get("/dog", function(req, res){
   res.send("MEOW");
 });
+
 // implement express route params asi "/r/:subReddit"
 // access request params via request object params
 app.get("/r/:subredditName", function(req, res){
   console.log(req.params);
   // { subredditName: 'dogs' }
+
   var subreddit = req.params.subredditName;
   res.send("welcome to the " + subreddit.toUpperCase() + " sub!");
 });
+
 // implement express route params asi "/r/:subReddit"
 app.get("/r/:subredditName/comments/:id/:title", function(req, res){
   console.log(req.params);
@@ -36,6 +41,7 @@ app.get("/r/:subredditName/comments/:id/:title", function(req, res){
 
   res.send("edgy " + req.params.title + " comment section");
 });
+
 // GET * is a catch-all for any URL aside from whatever else we've defined.
 // Good for error page. ORDER MATTERS, catch-all should live below
 // defined routes.
@@ -45,7 +51,6 @@ app.get("*", function(req, res){
 });
 
 // Tell express to listen for requests (start server)
-
 app.listen(3000, function(){
   console.log("engine rennin good too boy i tell u wHat");
 });
